3787f9793ce297a8f23fa429d1f729a745326916
akasatan <aka435304@gmail.com>
2021-02-14 02:23:07 +0000

    [Fix]migrateのimage_id削除

--- db/migrate/20210210064555_devise_create_users.rb
+++ db/migrate/20210210064555_devise_create_users.rb
@@ -34,7 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :name, null: false
       t.timestamps null: false
       t.text :introduction
-      t.string :image_id
+      t.string :profile_image_id
     end
 
     add_index :users, :email,                unique: true
